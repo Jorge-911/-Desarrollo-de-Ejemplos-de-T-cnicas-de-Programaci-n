4ee15196bce256651c4ea2df0a1b181d4f6f8815
Jorge-911 <142764663+Jorge-911@users.noreply.github.com>
2025-06-07 16:59:25 -0500

    Update Herencia.java

--- Herencia/Herencia.java
+++ Herencia/Herencia.java
@@ -1,17 +1,26 @@
+// Clase base llamada Animal
 class Animal {
+    // Método que puede ser sobreescrito por las subclases
     public void hacerSonido() {
         System.out.println("El animal hace un sonido");
     }
 }
 
-class Perro extends Animal {
+// Clase Herencia (simula ser un Perro) que hereda de Animal
+public class Herencia extends Animal {
+
+    // Sobrescribe el método hacerSonido() de la clase Animal
     @Override
     public void hacerSonido() {
         System.out.println("El perro ladra");
     }
 
+    // Método principal para ejecutar el ejemplo
     public static void main(String[] args) {
-        Perro miPerro = new Perro();
+        // Crear objeto de tipo Herencia
+        Herencia miPerro = new Herencia();
+
+        // Llamar al método sobreescrito
         miPerro.hacerSonido();
     }
 }
