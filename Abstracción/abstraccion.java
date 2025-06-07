ffb9dcc0539c55ba90d674e161abf4a60e75b0f8
Jorge-911 <142764663+Jorge-911@users.noreply.github.com>
2025-06-07 16:57:37 -0500

    Update abstraccion.java

--- Abstracción/abstraccion.java
+++ Abstracción/abstraccion.java
@@ -1,25 +1,36 @@
+// Clase abstracta que representa un Vehículo
 abstract class Vehiculo {
-    String marca;
+    String marca; // Atributo que guarda la marca del vehículo
 
+    // Constructor que asigna una marca al vehículo
     public Vehiculo(String marca) {
         this.marca = marca;
     }
 
+    // Método abstracto que obliga a las subclases a implementar conducir()
     public abstract void conducir();
 }
 
-class Coche extends Vehiculo {
-    public Coche(String marca) {
+// Clase concreta que extiende Vehiculo y le da funcionalidad al método conducir()
+public class Abstraccion extends Vehiculo {
+
+    // Constructor que llama al constructor de la clase padre
+    public Abstraccion(String marca) {
         super(marca);
     }
 
+    // Implementación del método abstracto conducir()
     @Override
     public void conducir() {
         System.out.println("Conduciendo un coche de marca " + marca);
     }
 
+    // Método principal para probar la clase
     public static void main(String[] args) {
-        Coche miCoche = new Coche("Toyota");
+        // Crear un objeto Abstraccion (tipo Coche) con marca "Toyota"
+        Abstraccion miCoche = new Abstraccion("Toyota");
+
+        // Llamar al método conducir() implementado
         miCoche.conducir();
     }
 }
